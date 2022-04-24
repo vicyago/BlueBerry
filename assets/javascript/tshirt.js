924500a5c5193a505780e1cbbc3860e2b2799356
vicyago <victoryagoc@gmail.com>
2022-04-24 20:01:13 -0300

    form changes

--- assets/javascript/tshirt.js
+++ assets/javascript/tshirt.js
@@ -80,10 +80,14 @@
    
 document.querySelector("#main-button").addEventListener("click", function(){
    document.querySelector("#divdisapear").style.display="none";
+   document.querySelector("#main-button2").disabled= true;
+   document.querySelector("#main-button2").style.cursor = "not-allowed";
    document.querySelector(".personalizado").style.display="block";
    
 })
 document.querySelector("#final-button").addEventListener("click", function () {
   document.querySelector(".personalizado").style.display="none";
+  document.querySelector("#main-button2").disabled = false;
+  document.querySelector("#main-button2").style.cursor = "default";
   document.querySelector("#divdisapear").style.display="block";
 });
