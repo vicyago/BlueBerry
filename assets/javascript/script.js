4c2f97236d32c6c3c6a16f49afd42f4ac1a0122e
vicyago <103522685+vicyago@users.noreply.github.com>
2022-06-01 02:52:59 -0300

    ada

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -29,11 +29,6 @@ btn.addEventListener("click", function () {
         btn.innerText = "Dark 🌙";
     }
     
-    const x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+    
 
 });
