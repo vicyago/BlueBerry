d262a8115ccbbfd5ffcf54f0aa89e2210b15ec2b
vicyago <103522685+vicyago@users.noreply.github.com>
2022-06-01 02:36:39 -0300

    aols

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,17 +20,12 @@ async function imageLoop() {
     imageLoop();
 }
 imageLoop();
-
 const btn = document.querySelector("#btn-toggle");
 btn.addEventListener("click", function () {
     document.body.classList.toggle("light-theme");
-      if (btn.innerText === "Dark 🌙") {
+    if (btn.innerText === "Dark 🌙") {
         btn.innerText = "Light ☀️";
     } else {
         btn.innerText = "Dark 🌙";
     }
-       
 });
-      
-  
-
