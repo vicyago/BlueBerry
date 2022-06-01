bbc5d32064cde194b7282dfd07a7e0c51defeedc
vicyago <103522685+vicyago@users.noreply.github.com>
2022-06-01 04:32:29 -0300

    trial finish

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -29,7 +29,16 @@ btn.addEventListener("click", function () {
         btn.innerText = "Dark 🌙";
     }
 })
-
+newFunction();
+function newFunction() {
+    const x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+;
 
 
 
