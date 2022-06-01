2885778c94ccb6bbcf175b566a9e2a4ae57ab8a4
vicyago <103522685+vicyago@users.noreply.github.com>
2022-06-01 02:58:41 -0300

    sgyA

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,4 +28,11 @@ btn.addEventListener("click", function () {
     } else {
         btn.innerText = "Dark 🌙";
     }
+    document.getElementById("myDIV");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+
 });
