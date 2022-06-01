f1d88dd895039e893aa905a6d66ac3f7019ecfb9
vicyago <103522685+vicyago@users.noreply.github.com>
2022-06-01 17:03:55 -0300

    uash

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,6 +20,7 @@ async function imageLoop() {
     imageLoop();
 }
 imageLoop();
+
 const btn = document.querySelector("#btn-toggle");
 btn.addEventListener("click", function () {
     document.body.classList.toggle("light-theme");
